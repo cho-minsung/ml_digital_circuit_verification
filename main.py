f9d6a17d4b469105d0c241fbcc0c9f947c88ec79
cms0426 <ms98.cho@gmail.com>
2021-11-14 20:04:16 -0800

    initial deep neural network tries

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # machine learning application to digital circuits verification
 import os
+import sys
 import subprocess
 import ltspice  # data parsing
 import matplotlib.pyplot as plt
 import pandas as pd
+pd.set_option('display.max_columns', None)
 import numpy as np
+np.set_printoptions(threshold=np.inf)
 from PyLTSpice.LTSpiceBatch import SimCommander
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
-if __name__ == '__main__':
+def get_data():
     schematic_path = "main.asc"
     inverter_path = "INV_20_10.asc"
     with open(inverter_path, 'r') as reader:
@@ -40,11 +47,11 @@ if __name__ == '__main__':
 
     increment = 50  # increment of 50ns
 
-    data = {"nmos length": np.zeros(10**4),
-            "nmos width": np.zeros(10**4),
-            "pmos length": np.zeros(10**4),
-            "pmos width": np.zeros(10**4),
-            "switching point": np.zeros(10**4)}
+    data = {"nmos length": np.zeros(10 ** 4),
+            "nmos width": np.zeros(10 ** 4),
+            "pmos length": np.zeros(10 ** 4),
+            "pmos width": np.zeros(10 ** 4),
+            "switching point": np.zeros(10 ** 4)}
 
     count = 0
     for h in range(10):  # nmos length sweep
@@ -92,6 +99,143 @@ if __name__ == '__main__':
     df.to_csv('data.csv')
 
 
+if __name__ == '__main__':
+    column_name = ['nmos length', 'nmos width', 'pmos length', 'pmos width', 'switching point']
+    raw_dataset = pd.read_csv('data.csv', names=column_name, skipinitialspace=True)
+    dataset = raw_dataset.copy()
+
+    train_dataset = dataset.sample(frac=0.8, random_state=0)
+    test_dataset = dataset.drop(train_dataset.index)
+
+    # sns.pairplot(train_dataset[['nmos length', 'nmos width', 'pmos length', 'pmos width']], diag_kind='kde')
+    # plt.show()
+
+    print(train_dataset.describe().transpose())
+
+    train_features = train_dataset.copy()
+    test_features = test_dataset.copy()
+
+    train_labels = train_features.pop('switching point')
+    test_labels = test_features.pop('switching point')
+
+    train_dataset.describe().transpose()
+
+    normalizer = tf.keras.layers.Normalization(axis=-1)
+    normalizer.adapt(np.array(train_features))
+    print(normalizer.mean.numpy())
+    first = np.array(train_features[:1])
+
+    with np.printoptions(precision=2, suppress=True):
+        print('First example:', first)
+        print('Normalized:', normalizer(first).numpy())
+
+    nmos_length = np.array(train_features['nmos length'])
+    nmos_length_normalizer = layers.Normalization(input_shape=[1, ], axis=None)
+    nmos_length_normalizer.adapt(nmos_length)
+
+    nmos_length_model = tf.keras.Sequential([
+        nmos_length_normalizer,
+        layers.Dense(units=1)
+    ])
+
+    nmos_length_model.summary()
+
+    print(nmos_length_model.predict(nmos_length[:10]))
+
+    nmos_length_model.compile(
+        optimizer=tf.optimizers.Adam(learning_rate=0.1),
+        loss='mean_absolute_error'
+    )
+
+    history = nmos_length_model.fit(
+        train_features['nmos length'],
+        train_labels,
+        epochs=100,
+        # Suppress logging.
+        verbose=0,
+        # Calculate validation results on 20% of the training data.
+        validation_split=0.2)
+
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+    print(hist.tail())
+
+    def plot_loss(history):
+        plt.plot(history.history['loss'], label='loss')
+        plt.plot(history.history['val_loss'], label='val_loss')
+        plt.ylim([0, 0.02])
+        plt.xlabel('Epoch')
+        plt.ylabel('Error [switching point]')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    plot_loss(history)
+
+    test_results = {}
+    test_results['nmos_length'] = nmos_length_model.evaluate(
+        test_features['nmos length'],
+        test_labels, verbose=0
+    )
+
+    x = tf.linspace(0.0, 250, 251)
+    y = nmos_length_model.predict(x)
 
 
+    def plot_nmos_length(x, y):
+        plt.scatter(train_features['nmos length'], train_labels, label='Data')
+        plt.plot(x, y, color='k', label='Predictions')
+        plt.xlabel('nmos length')
+        plt.ylabel('switching point')
+        # plt.ylim(top=max(y), bottom=0)
+        plt.legend()
+        plt.show()
 
+    plot_nmos_length(x, y)
+
+    linear_model = tf.keras.Sequential([
+        normalizer,
+        layers.Dense(units=1)
+    ])
+
+    linear_model.predict(train_features[:10])
+
+    print(linear_model.layers[1].kernel)
+
+    linear_model.compile(
+        optimizer=tf.optimizers.Adam(learning_rate=0.1),
+        loss='mean_absolute_error')
+
+    history = linear_model.fit(
+        train_features,
+        train_labels,
+        epochs=100,
+        # Suppress logging.
+        verbose=0,
+        # Calculate validation results on 20% of the training data.
+        validation_split=0.2)
+
+    plot_loss(history)
+
+
+    def build_and_compile_model(norm):
+        model = keras.Sequential([
+            norm,
+            layers.Dense(64, activation='relu'),
+            layers.Dense(64, activation='relu'),
+            layers.Dense(1)
+        ])
+        model.compile(loss='mean_absolute_error',
+                      optimizer=tf.keras.optimizers.Adam(0.001))
+        return model
+
+    dnn_model = build_and_compile_model(normalizer)
+    print(dnn_model.summary())
+
+    history = dnn_model.fit(
+        train_features,
+        train_labels,
+        validation_split=0.2,
+        verbose=0, epochs=100)
+
+    plot_loss(history)
